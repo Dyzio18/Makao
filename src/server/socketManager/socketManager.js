efe2afc79da8b2c8c97eec0883048727453dc0bc
dyzio18 <dyzio18@gmail.com>
2017-12-01 16:31:31 +0100

    add new socket endpoints, user-name

--- src/server/socketManager/socketManager.js
+++ src/server/socketManager/socketManager.js
@@ -1,14 +1,29 @@
 import {io} from './../index';
 
+// number of user in room
+let userCountInRoom = 0;
 
-function socketManager(io){
+function socketManager(io) {
 
-    io.on('message', body => {
+    io.on('connect', () => {
+        userCountInRoom++;
+        console.log(userCountInRoom);
+    });
+
+    /**
+     * From
+     */
+    io.on('message', message => {
         io.broadcast.emit('message', {
-            body,
-            from: io.id.slice(8)
+            body: message.body,
+            from: message.from
         })
+    });
+
+    io.on('nick-name', nick => {
+
     })
+
 }
 
 
